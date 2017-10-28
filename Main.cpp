9b7b9da0665bb3bb6add96172ac488134ef02ea9
FrozenIce <icecastle221@gmail.com>
2017-10-28 15:50:55 -0400

    Fix debug main.

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,10 @@
 
-#if _PC
+#include <crtdbg.h>  
+
+#if defined(_PC)  
+
 #include "WindowsEngine.h"
 #include <Windows.h>
-//typedef float gFloat;
-#endif
-
-#if _PS4
-// PS4 Resource Manager or Material Manager
-#endif
 
 // --------------------------------------------------------
 // Entry point for a graphical (non-console) Windows application
@@ -22,11 +19,10 @@ int WINAPI WinMain(
 	// Enable memory leak detection as a quick and dirty
 	// way of determining if we forgot to clean something up
 	//  - You may want to use something more advanced, like Visual Leak Detector
-	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	//_CrtSetBreakAlloc(1430);
 #endif
 
-#if _PC
 	WindowsEngine engine = WindowsEngine(
 		hInstance,		   // The application's handle
 		"DirectX Game",	   // Text for the window's title bar
@@ -35,32 +31,25 @@ int WINAPI WinMain(
 		true);			   // Show extra stats (fps) in title bar?
 
 	engine.Run();
+
+
+#elif defined(_PS4)  
+// PS4 Resource Manager or Material Manager
+int main() {
+
+#if defined(DEBUG) | defined(_DEBUG)
+	// Enable memory leak detection as a quick and dirty
+	// way of determining if we forgot to clean something up
+	//  - You may want to use something more advanced, like Visual Leak Detector
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetBreakAlloc(1430);
 #endif
 
-#if _PS4
-	// PS4 Resource Manager or Material Manager
+#else  
+
+int main() {
+
 #endif
-
-	//// Create the Game object using
-	//// the app handle we got from WinMain
-	//WindowsRenderSystem dxGame(hInstance);
-
-	//// Result variable for function calls below
-	//HRESULT hr = S_OK;
-
-	//// Attempt to create the window for our program, and
-	//// exit early if something failed
-	//hr = dxGame.InitWindow();
-	//if(FAILED(hr)) return hr;
-
-	//// Attempt to initialize DirectX, and exit
-	//// early if something failed
-	//hr = dxGame.InitDirectX();
-	//if(FAILED(hr)) return hr;
-
-	//// Begin the message and game loop, and then return
-	//// whatever we get back once the game loop is over
-	//return dxGame.Run();
 
 	return 0;
 }
