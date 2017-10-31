1f60d32e2770bf763574f79a42e058c8d49f9b10
xiarui22 <xiarui22@gmail.com>
2017-10-31 17:31:38 -0400

    glm Vertex

--- Vertex.h
+++ Vertex.h
@@ -1,18 +1,26 @@
 #pragma once
 
-#include <DirectXMath.h>
+//#include <DirectXMath.h>
 
-//#include "glm\glm.hpp"
+#include "glm\glm.hpp"
 
 // --------------------------------------------------------
 // A custom vertex definition
 //
 // You will eventually ADD TO this, and/or make more of these!
 // --------------------------------------------------------
+//struct Vertex
+//{
+//	DirectX::XMFLOAT3 Position;	    // The position of the vertex
+//	DirectX::XMFLOAT2 UV;			// UV location
+//	DirectX::XMFLOAT3 Normal;		// Normal direction
+//	DirectX::XMFLOAT3 Tangent;		// For normal mapping
+//};
+
 struct Vertex
 {
-	DirectX::XMFLOAT3 Position;	    // The position of the vertex
-	DirectX::XMFLOAT2 UV;			// UV location
-	DirectX::XMFLOAT3 Normal;		// Normal direction
-	DirectX::XMFLOAT3 Tangent;		// For normal mapping
+	glm::vec3 Position;	    // The position of the vertex
+	glm::vec2 UV;			// UV location
+	glm::vec3 Normal;		// Normal direction
+	glm::vec3 Tangent;		// For normal mapping
 };
